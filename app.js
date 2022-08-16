743ed4b58a1dfa1fbef55d6e1233a59c38181152
saiem-jatri <shahedsaiem.jatri@gmail.com>
2022-08-16 17:36:15 +0600

    pagination updated

--- app.js
+++ app.js
@@ -4,29 +4,29 @@ const app = Vue.createApp({
       searchString: '',
         inputTask:'',
         showMessege:false,
-        nextId:14,
+        // nextId:14,
         currentPage:0,
         pageSize:3,
         visibleTodos:[],
         
         todos: [
-          {id:1,name:"shahed",isComplete:false},
-          {id:2,name:"saiem",isComplete:false},
-          {id:3,name:"mohaimen",isComplete:true},
-          {id:4,name:"mohaimen",isComplete:true},
-          {id:5,name:"mohaimen",isComplete:true},
-          {id:6,name:"mohaimen",isComplete:true},
-          {id:7,name:"mohaimen",isComplete:true},
-          {id:8,name:"mohaimen",isComplete:true},
-          {id:9,name:"mohaimen",isComplete:true},
-          {id:10,name:"mohaimen",isComplete:true},
-          {id:11,name:"mohaimen",isComplete:true},
-          {id:12,name:"mohaimen",isComplete:true},
-          {id:13,name:"mohaimen",isComplete:true},
+          {name:"shahed",isComplete:false},
+          {name:"saiem",isComplete:false},
+          {name:"mohaimen",isComplete:true},
+          {name:"mohaimen",isComplete:true},
+          {name:"mohaimen",isComplete:false},
+          {name:"mohaimen",isComplete:true},
+          {name:"mohaimen",isComplete:true},
+          {name:"mohaimen",isComplete:true},
+          {name:"mohaimen",isComplete:false},
+          {name:"mohaimen",isComplete:true},
+          {name:"mohaimen",isComplete:false},
+          {name:"mohaimen",isComplete:true},
+          {name:"mohaimen",isComplete:false},
         ],
      };
   },
-  beforeMount() {
+  created() {
     this.updateVisibleTodos();
   },
   methods: {
@@ -57,8 +57,9 @@ updatePage(pageNumber){
 },
 
 updateVisibleTodos(){
-   this.visibleTodos = this.todos.slice(this.currentPage * this.pageSize, (this.currentPage * this.pageSize) + this.pageSize);
-    // if there are no visible todos, go back page
+   this.visibleTodos = this.filterItems.slice(this.currentPage * this.pageSize, (this.currentPage * this.pageSize) + this.pageSize);
+   console.log(this.visibleTodos) 
+   // if there are no visible todos, go back page
     if(this.visibleTodos.length == 0 && this.currentPage > 0){
       this.updatePage(this.currentPage - 1);
     }
@@ -67,16 +68,21 @@ updateVisibleTodos(){
 
   computed: {
     filterItems(){
-      return this.visibleTodos.filter(items=> items.name.toLowerCase().includes(this.searchString))
+      if(this.searchString == '') {
+        return this.todos;
+      } else {
+        return this.todos.filter(items=> items.name.toLowerCase().includes(this.searchString))
+      }
+      
     },
 
     afterFilter(){
       return this.filterItems.filter(items =>{
         return items.isComplete === true;
-      } )
+      })
     },
     totalPages(){
-     return Math.ceil(this.todos.length / this.pageSize)
+     return Math.ceil(this.filterItems.length / this.pageSize)
     },
     showPreviousLink(){
       return this.currentPage == 0 ? false : true;
